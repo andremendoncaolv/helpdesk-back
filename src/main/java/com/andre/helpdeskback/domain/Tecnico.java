2d2e78ec88cee2b431fb14adf6127cc4c04cdc97
andremendoncaolv <andremendoncaolv@gmail.com>
2023-09-22 17:13:23 -0300

    Protegendo aplicação contra serealização

--- src/main/java/com/andre/helpdeskback/domain/Tecnico.java
+++ src/main/java/com/andre/helpdeskback/domain/Tecnico.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.andre.helpdeskback.domain.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
@@ -14,6 +15,7 @@ public class Tecnico extends Pessoa implements Serializable{
 
     private static final long serialVersionUID = 1L;
 	
+    @JsonIgnore
     @OneToMany(mappedBy = "tecnico")
 	private List<Chamado> chamados = new ArrayList<>();
 
