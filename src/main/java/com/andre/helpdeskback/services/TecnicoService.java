5e512c44e481d34423d87cb508a498ee19874c5f
andremendoncaolv <andremendoncaolv@gmail.com>
2023-09-25 11:43:24 -0300

    Endpoint findAll para Tecnico

--- src/main/java/com/andre/helpdeskback/services/TecnicoService.java
+++ src/main/java/com/andre/helpdeskback/services/TecnicoService.java
@@ -1,11 +1,13 @@
 package com.andre.helpdeskback.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andre.helpdeskback.domain.Tecnico;
+import com.andre.helpdeskback.domain.dtos.TecnicoDTO;
 import com.andre.helpdeskback.repositories.TecnicoRepository;
 import com.andre.helpdeskback.services.exceptions.ObjectnotFoundException;
 
@@ -18,6 +20,10 @@ public class TecnicoService {
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id:" + id));
+	}
+
+	public List<Tecnico> findAll() {
+		return repository.findAll();
 	}
 
 }
