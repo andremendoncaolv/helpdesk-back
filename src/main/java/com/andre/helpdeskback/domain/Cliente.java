0d300e9092699e567bbfa89195fad6c31b25b233
andremendoncaolv <andremendoncaolv@gmail.com>
2023-09-26 15:48:28 -0300

    Crud para cliente emplementado

--- src/main/java/com/andre/helpdeskback/domain/Cliente.java
+++ src/main/java/com/andre/helpdeskback/domain/Cliente.java
@@ -3,7 +3,9 @@ package com.andre.helpdeskback.domain;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import com.andre.helpdeskback.domain.dtos.ClienteDTO;
 import com.andre.helpdeskback.domain.enums.Perfil;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -27,6 +29,17 @@ public class Cliente extends Pessoa implements Serializable{
 	public Cliente(Integer id, String nome, String email, String cpf, String senha) {
 		super(id, nome, email, cpf, senha);
 		addPerfil(Perfil.CLIENTE);
+	}
+	
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+;		this.email = obj.getEmail();
+		this.cpf = obj.getCpf();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public List<Chamado> getChamados() {
